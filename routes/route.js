e5123023196d21a61420a8ba1c24608eac8f5f8a
Chennappan <Kamalakannan.Chennappan@Mattel.com>
2017-06-07 17:56:32 -0700

    Added videoplayer page

--- routes/route.js
+++ routes/route.js
@@ -1,22 +1,31 @@
 const express = require('express');
+const axios = require('axios');
 
 var app = new express();
 var router = express.Router();
 var appTitle = "JW Player POC";
+var playlistUrl = "https://cdn.jwplayer.com/v2/playlists/Mx7YyYDQ";
 
 router.get('/',(req,res)=>{
   res.render('playlist.hbs',{
     appTitle: appTitle,
     pageTitle: 'Playlist | Mattel',
-    hasNav: true,
+    hasNav: true
   });
 });
 
 router.get('/mattelvideoplayer',(req,res)=>{
-  res.render('mattelvideoplayer.hbs',{
-    appTitle: appTitle,
-    pageTitle: 'Playlist | Mattel',
-    hasNav: true,
+  axios.get(playlistUrl).then((response)=>{
+    // console.log(response.data.playlist);
+    res.render('mattelvideoplayer.hbs',{
+      appTitle: appTitle,
+      pageTitle: 'Playlist | Mattel',
+      hasNav: true,
+      playlist: response.data.playlist
+    });
+  }).catch((error)=>{
+    console.log(error);
+    res.redirect('/');
   });
 });
 
